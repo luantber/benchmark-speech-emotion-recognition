135abfdc91e2a973b04b89082f3bb19fd75ff5e7
luis <luisbch9@gmail.com>
2020-07-27 19:11:01 -0500

    fix

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from dataset.ravdess import Ravdess
-from models.transforms import to_spectrogram_compose
+from models.transforms import to_spectrogram_compose_norandom
 from torch.utils.data import DataLoader
 from models.cnn import CNN
 
@@ -9,16 +9,5 @@ path = "lightning_logs/version_17/checkpoints/epoch=181.ckpt"
 model_cnn = CNN.load_from_checkpoint(path)
 
 
-r = Ravdess(train=False,transform=to_spectrogram_compose)
-r.benchmark(model_cnn,"cnn_rect_fine")
-r.benchmark(model_cnn,"cnn_rect_fine")
-
-
-# data_test = DataLoader( Ravdess(train=False,transform=to_spectrogram_compose()) , batch_size=32 )
-# for x,y in data_test:
-#     print (x.size())
-# algo = r[0][0]
-# print( algo )
-# print ( model_cnn(algo.view(1,1,128,128))  ) 
-
-# print ( r[0][1])
+r = Ravdess(train=False,transform=to_spectrogram_compose_norandom)
+r.benchmark(model_cnn,"cnn_rect_fine_norand")
