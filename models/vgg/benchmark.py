708ac4eafd6ddae1e72ffb3d399bc230a877cb38
Luis Bernal <luisbch9@gmail.com>
2020-10-10 21:21:04 -0500

    VGG

--- models/vgg/benchmark.py
+++ models/vgg/benchmark.py
@@ -1,10 +1,10 @@
-from models.vgg.cnn_model import CNNModel
+from models.vgg.model import VGG
 from models.vgg.prepro import preprocesar_audio
 from ser_bench.challenges.v1 import ravdess_bench
 
 
-model = CNNModel.load_from_checkpoint(
-    'models/vgg/lightning_logs/version_1/checkpoints/epoch=13.ckpt')
+model = VGG.load_from_checkpoint(
+    'models/vgg/lightning_logs/version_0/checkpoints/epoch=23.ckpt')
 
 resultados = ravdess_bench(model, "VGG", preprocesar_audio)
 resultados.write()
